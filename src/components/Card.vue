8980733312d88193f80ba30fbe6b6951895cd3b1
Egor <egorviliavin@gmail.com>
2024-02-11 22:30:10 +0300

    Добавляет компонент Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="border p-5 rounded-xl w-64 shadow-md transition-all hover:-translate-y-2"
+  >
+    <img src="../public/heart.svg" alt="favorites" class="cursor-pointer" />
+    <img
+      src="../public/sneakers/sneakers-1.jpg"
+      alt="sneakers photo"
+      class="w-36 mx-auto"
+    />
+    <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+    <div class="flex justify-between items-start mt-2">
+      <ul class="flex flex-col items-start">
+        <li><span class="text-gray-500">Цена:</span></li>
+        <li><p class="font-bold">12 999 руб.</p></li>
+      </ul>
+      <img src="../public/plus.svg" alt="Plus" class="cursor-pointer" />
+    </div>
+  </div>
+</template>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,3 +1,7 @@
+<script setup>
+  import Card from './Card.vue';
+</script>
+
 <template>
   <div class="container px-10">
     <div class="flex justify-between items-center pb-5">
@@ -15,24 +19,7 @@
       </div>
     </div>
     <div class="flex flex-wrap py-5 justify-between">
-      <div
-        class="border p-5 rounded-xl w-64 shadow-md transition-all hover:-translate-y-2"
-      >
-        <img src="../public/heart.svg" alt="favorites" class="cursor-pointer" />
-        <img
-          src="../public/sneakers/sneakers-1.jpg"
-          alt="sneakers photo"
-          class="w-36 mx-auto"
-        />
-        <p>Мужские кроссовки Nike Blazer Mid Suede</p>
-        <div class="flex justify-between items-start mt-2">
-          <ul class="flex flex-col items-start">
-            <li><span class="text-gray-500">Цена:</span></li>
-            <li><p class="font-bold">12 999 руб.</p></li>
-          </ul>
-          <img src="../public/plus.svg" alt="Plus" class="cursor-pointer" />
-        </div>
-      </div>
+      <Card />
     </div>
   </div>
 </template>
